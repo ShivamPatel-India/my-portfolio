747dba5948107409c33d1ba6432b146485772344
shivam1209 <shivampatel.sp515@gmail.com>
2023-12-03 01:54:18 -0800

    Initial commit of react app made with vite. This commit contain chnages to render main 3d island and to render it. Code to add the 3D plane and bird are also added.

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import  {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { Home, About, Projects, Contact } from './Pages';
+const App = () => {
+  return (
+    <main className='bg-slate-300/20'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
+      </Router>
+    </main>
+  )
+}
+
+export default App
